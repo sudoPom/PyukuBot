6283f82847af5eaa2524b83326296db678c76c2a
sudoPom <zcabofe@ucl.ac.uk>
2021-07-27 22:59:13 +0100

    Feat: Added a new type of command that accepts params

--- src/commands/flip.py
+++
@@ -1,8 +0,0 @@
-import random
-from decorators import static_command
-description = "flips a coin"
-
-@static_command
-def main():
-    choices = ["HEADS", "TAILS"]
-    return f"The coin landed on: {random.choice(choices)}"
--- src/commands/roll.py
+++
@@ -1,9 +0,0 @@
-import random
-from decorators import named_static_command
-
-description = "rolls a number between 0 and 100"
-
-@named_static_command
-def main(name):
-    num = random.randint(0,100)
-    return f"{name} rolled a number: {num}"
